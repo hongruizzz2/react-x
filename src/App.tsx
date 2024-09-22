f471f04d92fed93e464740b2e0dc42ea9bfde4cf
hongrui <1169948065@qq.com>
2024-09-22 23:26:08 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+      <div className="App">
+          <p>
+              欢迎使用vuact-cli脚手架创建React+TS
+          </p>
+          <p>
+              Author：洪睿
+          </p>
+          <a
+              className="App-link"
+              href="https://github.com/hongruizzz2"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+              项目Github地址
+          </a>
+      </div>
+  );
+}
+
+export default App;
